3334bf8cb6795aa0d19ffa4423f5b17a13124f8e
takuto-tsukigata <takuto.tsukigata@rakus-partners.co.jp>
2022-02-07 15:58:51 +0900

    管理者登録画面を表示する処理を作成

---
+++ src/main/java/jp/co/sample/controller/AdministratorController.java
@@ -0,0 +1,30 @@
+package jp.co.sample.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import jp.co.sample.form.InsertAdministratorForm;
+import jp.co.sample.service.AdministratorService;
+
+@Controller
+@RequestMapping("/administrator")
+public class AdministratorController {
+
+	@Autowired
+	private AdministratorService administratorService;
+
+	/**
+	 * @return
+	 */
+	@ModelAttribute
+	public InsertAdministratorForm setUpInsertAdministratorForm() {
+		return new InsertAdministratorForm();
+	}
+
+	@RequestMapping("/toInsert")
+	public String toInsert() {
+		return "insert";
+	}
+}
