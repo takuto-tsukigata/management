d1c6fbd77ff41df29652785ac1c18c8d34c860f9
takuto-tsukigata <takuto.tsukigata@rakus-partners.co.jp>
2022-02-07 17:41:56 +0900

    AdministratorControllerクラスを修正しました

--- src/main/java/jp/co/sample/controller/AdministratorController.java
+++ src/main/java/jp/co/sample/controller/AdministratorController.java
@@ -1,8 +1,11 @@
 package jp.co.sample.controller;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -34,6 +37,9 @@ public class AdministratorController {
 		return new LoginForm();
 	}
 
+	@Autowired
+	private HttpSession session;
+
 	/**
 	 * 管理者情報登録画面
 	 * 
@@ -59,5 +65,19 @@ public class AdministratorController {
 	@RequestMapping("/")
 	public String toLogin() {
 		return "administrator/login";
+	}
+
+	@RequestMapping("/login")
+	public String login(LoginForm form, Model model) {
+
+		Administrator administrator = administratorService.login("mailAddress", "password");
+
+		if (administrator == null) {
+			model.addAttribute("loginError", "メールアドレスまたはパスワードが不正です");
+			return "administrator/login";
+		} else {
+			session.setAttribute("administratorName", administrator.getName());
+			return "forward:/employee/showList";
+		}
 	}
 }
