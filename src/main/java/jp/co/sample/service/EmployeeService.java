2c5c1856650a5967f319a66b693bbf401a82b2f9
takuto-tsukigata <takuto.tsukigata@rakus-partners.co.jp>
2022-02-08 15:35:34 +0900

    EmployeeServiceクラスにupdate()メソッドを追加しました

--- src/main/java/jp/co/sample/service/EmployeeService.java
+++ src/main/java/jp/co/sample/service/EmployeeService.java
@@ -24,4 +24,9 @@ public class EmployeeService {
 	public Employee showDetail(Integer id) {
 		return employeeRepository.load(id);
 	}
+
+	public void update(Employee employee) {
+		employeeRepository.update(employee);
+	}
+
 }
